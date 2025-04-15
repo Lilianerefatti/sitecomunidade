03bfbba09290cc3fd380a9d04db60b9d52c078db
Liliane Refatti <lilianerefatti@hotmail.com>
2025-04-15 12:25:13 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
 from comunidade import app
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
-
+    port = int(os.environ.get("PORT", 5000))  # usa 5000 localmente e PORT no Railway
+    app.run(debug=True, host='0.0.0.0', port=port)

24e122d1e97a6adb34605f33d14639061f86b305
Liliane Refatti <lilianerefatti@hotmail.com>
2025-04-15 11:32:57 -0300

    Update __init__.py

--- comunidade/__init__.py
+++ comunidade/__init__.py
@@ -6,17 +6,21 @@ from flask_login import LoginManager
 import os
 import sqlalchemy
 
+import sqlalchemy
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+import os
+import sqlalchemy
+
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'deda5200337483fd9ba3bcfae9008533'
 if os.getenv("DATABASE_URL"):
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 else:
-    #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
-    # Caminho absoluto para o banco
-    basedir = os.path.abspath(os.path.dirname(__file__))
-    db_path = os.path.join(basedir, '..', 'instance', 'comunidade.db')
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
 
 # Inicializa extensões
 database = SQLAlchemy(app)
@@ -31,7 +35,7 @@ inspector = sqlalchemy.inspect(engine)
 if not inspector.has_table("usuario"):
 # Cria as tabelas se ainda não existirem
     with app.app_context():
-        database.DROP_ALL()
+        database.drop_all()
         database.create_all()
         print("Base de dados criado")
 else:
